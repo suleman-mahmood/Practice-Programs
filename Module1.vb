3cf399a68953027ba69ffaa4f19977c367213a08
Suleman Mahmood <sulemanmahmood99@gmail.com>
2018-03-23 14:36:57 +0500

    Initial Commit

---
+++ Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim x, i, factorial As Integer
+        factorial = 1
+
+        x = Console.ReadLine
+
+        For i = 1 To x
+            factorial = factorial * i
+        Next
+
+        Console.WriteLine("The factorial of " & x & " is :- " & factorial)
+
+        Console.ReadKey()
+    End Sub
+
+End Module
