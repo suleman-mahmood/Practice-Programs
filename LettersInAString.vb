ecad3011f79e08f829c3d6c7ab85517665ef3fbc
Suleman Mahmood <sulemanmahmood99@gmail.com>
2019-02-01 17:28:46 +0500

    This program checks whether a given string is entirely made up of only I,O,S,H,Z,X,N

---
+++ LettersInAString.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+    ' Q) Check whether a given string is entirely made up of Only I,O,S,H,Z,X,N
+    Sub Main()
+        Dim input As String
+        Dim check As Char
+        Dim isValid = True
+
+        input = Console.ReadLine
+
+        For i = 1 To Len(input) And isValid
+            check = input(i - 1)
+            If checkValidity(check) Then
+                isValid = True
+            Else
+                isValid = False
+            End If
+        Next
+
+        If isValid = False Then
+            Console.WriteLine("NO")
+        ElseIf isValid = True Then
+            Console.WriteLine("YES")
+        End If
+        Console.ReadKey()
+    End Sub
+
+    Function checkValidity(ByVal check As Char) As Boolean
+        If check = "I" Or check = "O" Or check = "S" Or check = "H" Or check = "Z" Or check = "X" Or check = "N" Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+End Module
