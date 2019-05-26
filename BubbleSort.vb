b734ae3e81a2b2aeece00c4230bfe12567424a14
Suleman Mahmood <sulemanmahmood99@gmail.com>
2019-05-25 18:06:24 -0700

    BubbleSort

---
+++ BubbleSort.vb
@@ -0,0 +1,57 @@
+﻿Module module1
+
+    Sub Main()
+        Dim numbers(99) As Integer
+        Dim randomNumber As Random
+
+        randomNumber = New Random
+
+        'Filling up the array with random values between 1 and 100
+        For i = 0 To 99
+            numbers(i) = (randomNumber.Next() Mod 100) + 1
+        Next
+
+        'Lets output the elements of the array
+        Console.WriteLine("Before Sorting:")
+        For i = 0 To 99
+            Console.WriteLine(numbers(i))
+        Next
+
+        'Calling the insert sort Procedure by passing my array as
+        'a reference so that it comes back sorted
+        Call BubbleSort(numbers, 100)
+
+        'Lets output the elements of the array
+        Console.WriteLine("After Sorting:")
+        For i = 0 To 99
+            Console.WriteLine(numbers(i))
+        Next
+
+        Console.ReadKey()
+    End Sub
+
+    Sub BubbleSort(ByRef numbers() As Integer, ByVal sizeOfArray As Integer)
+
+        Dim hasSwapped As Boolean
+        Dim temp As Integer
+
+        hasSwapped = True
+
+        While hasSwapped
+
+            hasSwapped = False
+
+            For i = 0 To (sizeOfArray - 2)
+                If numbers(i) > numbers(i + 1) Then
+                    hasSwapped = True
+                    temp = numbers(i)
+                    numbers(i) = numbers(i + 1)
+                    numbers(i + 1) = temp
+                End If
+            Next
+
+        End While
+
+    End Sub
+
+End Module
